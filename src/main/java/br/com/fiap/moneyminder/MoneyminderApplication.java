7ce3e534d48216cfa7949696cf69824eadcf8103
Mateus Fattori <83764798+MateusFattori@users.noreply.github.com>
2024-02-26 08:02:03 -0300

    Add files via upload

--- src/main/java/br/com/fiap/moneyminder/MoneyminderApplication.java
+++ src/main/java/br/com/fiap/moneyminder/MoneyminderApplication.java
@@ -2,8 +2,11 @@ package br.com.fiap.moneyminder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class MoneyminderApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +17,6 @@ public class MoneyminderApplication {
 	public String home(){
 		return "ok";
 	}
+
 
 }
